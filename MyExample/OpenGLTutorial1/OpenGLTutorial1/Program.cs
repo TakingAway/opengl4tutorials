ab66cf7c39ee546354e128fba1c6c80bb5cffd86
Bogdanov Dmitry <bogdanovdi888@gmail.com>
2019-05-05 00:56:15 +0300

    Upd
    Upd

---
+++ MyExample/OpenGLTutorial1/OpenGLTutorial1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTutorial1
+{
+    class Program
+    {
+        private static int width = 1200, height = 720;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("Open GL Tutorial 1");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnRenderFrame()
+        {
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Glut.glutSwapBuffers();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+    }
+}
